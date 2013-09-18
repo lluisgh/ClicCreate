62a343bf71d42a25d8413dcb502fe1dadd0b7b13
Lluis Gomez <lluisgh28@gmail.com>
2013-09-18 18:58:49 +0200

    Initial commit
    Signed-off-by: Lluis Gomez <lluisgh28@gmail.com>

---
+++ ClicCreate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClicCreate
+//
+//  Created by Lluís Gómez Hernando on 18/09/13.
+//  Copyright (c) 2013 Lluís Gómez Hernando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LGHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LGHAppDelegate class]));
+    }
+}
